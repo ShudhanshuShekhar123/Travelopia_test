dd9e22bcf38118406c28a3213bc39d3c762f6ee3
shudhanshuShekhar <shudhanshuraz01@gmail.com>
2023-12-06 14:24:24 +0530

    adding responsiveness and toggle feture

---
+++ script/index.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    var menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+  }
+
+
+  
+  function togglebtn(){
+   
+     let mobile = document.querySelector(".more_description-mobile")
+    
+      mobile.classList.toggle("showed")
+
+
+  }
+ 
